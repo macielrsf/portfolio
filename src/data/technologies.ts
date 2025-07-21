4c48593e1b523d3fb359dfa47efa8ccb3d7d4ca3
Maciel Rodrigues <macielrsf@gmail.com>
2025-07-21 20:39:54 -0300

    technologies images

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,109 +1,157 @@
-import { Technology } from '../types/Technology'
+import { Technology } from "../types/Technology";
+
+import reduxIcon from '../assets/technologies/redux.png';
+import reactIcon from '../assets/technologies/react.png';
+import typescriptIcon from '../assets/technologies/typescript.png';
+import nodeIcon from '../assets/technologies/node.png';
+import laravelIcon from '../assets/technologies/laravel.png';
+import phpIcon from '../assets/technologies/php.png';
+import pythonIcon from '../assets/technologies/python.png';
+import dockerIcon from '../assets/technologies/docker.png';
+import jestIcon from '../assets/technologies/jest.png';
+import javascriptIcon from '../assets/technologies/javascript.png';
+import htmlIcon from '../assets/technologies/html.png';
+import cssIcon from '../assets/technologies/css.png';
+import awsIcon from '../assets/technologies/aws.png';
+import postgresqlIcon from '../assets/technologies/postgresql.png';
+import fastlaneIcon from '../assets/technologies/fastlane.png';
+import firebaseIcon from '../assets/technologies/firebase.png';
+import gitIcon from '../assets/technologies/git.png';
+import figmaIcon from '../assets/technologies/figma.png';
+import viteIcon from '../assets/technologies/vite.png';
 
 export const technologies: Technology[] = [
-    {
-      "_id": "682cfe1d34e57a1daab7731f",
-      "name": "NodeJs",
-      "slug": "nodejs",
-      "icon": "https://imgur.com/EmtPQW1.png",
-      "appIcon": "https://imgur.com/ywXMsOQ.png"
-    },
-    {
-      "_id": "682d009c34e57a1daab77345",
-      "name": "JS",
-      "slug": "js",
-      "icon": "https://imgur.com/coJyCvz.png",
-      "appIcon": "https://imgur.com/a0bqQOr.png"
-    },
-    {
-      "_id": "682d013b34e57a1daab77351",
-      "name": "Spring Boot",
-      "slug": "spring-boot",
-      "icon": "https://imgur.com/Znni0bB.png",
-      "appIcon": "https://imgur.com/NRo85Nm.png"
-    },
-    {
-      "_id": "682d0b50e0e8b8ba4134df5b",
-      "name": "React",
-      "slug": "react",
-      "icon": "https://imgur.com/OQCWJCa.png",
-      "appIcon": "https://imgur.com/c3VGkqF.png"
-    },
-    {
-      "_id": "682d0c21e0e8b8ba4134df77",
-      "name": "Java",
-      "slug": "java",
-      "icon": "https://imgur.com/PEX5yko.png",
-      "appIcon": "https://imgur.com/D9uzdP1.png"
-    },
-    {
-      "_id": "682d0c37e0e8b8ba4134df7d",
-      "name": "Python",
-      "slug": "python",
-      "icon": "https://imgur.com/0EoYdRN.png",
-      "appIcon": "https://imgur.com/OLAA4qV.png"
-    },
-    {
-      "_id": "682d0d66e0e8b8ba4134df83",
-      "name": "Mongodb",
-      "slug": "mongodb",
-      "icon": "https://imgur.com/s8Nf4Z7.png",
-      "appIcon": "https://imgur.com/YUsmpip.png"
-    },
-    {
-      "_id": "682d0d81e0e8b8ba4134df89",
-      "name": "Mysql",
-      "slug": "mysql",
-      "icon": "https://imgur.com/wQbrTTs.png",
-      "appIcon": "https://imgur.com/Bk0nTm7.png"
-    },
-    {
-      "_id": "682d0dc1e0e8b8ba4134df93",
-      "name": "PosgreSQL",
-      "slug": "posgresql",
-      "icon": "https://imgur.com/vZ40APo.png",
-      "appIcon": "https://imgur.com/scDHVnP.png"
-    },
-    {
-      "_id": "682d11b0e0e8b8ba4134df99",
-      "name": "TS",
-      "slug": "ts",
-      "icon": "https://imgur.com/a4BXK2a.png",
-      "appIcon": "https://imgur.com/qTeONsE.png"
-    },
-    {
-      "_id": "682d11c5e0e8b8ba4134df9f",
-      "name": "Redis",
-      "slug": "redis",
-      "icon": "https://imgur.com/18BBZLH.png",
-      "appIcon": "https://imgur.com/Rjajass.png"
-    },
-    {
-      "_id": "682d11dce0e8b8ba4134dfa5",
-      "name": "Docker",
-      "slug": "docker",
-      "icon": "https://imgur.com/zO3Dwmq.png",
-      "appIcon": "https://imgur.com/IBQbWb7.png"
-    },
-    {
-      "_id": "682d11eae0e8b8ba4134dfab",
-      "name": "CSS",
-      "slug": "css",
-      "icon": "https://imgur.com/m7X3kJ8.png",
-      "appIcon": "https://imgur.com/UjqHbrk.png"
-    },
-    {
-      "_id": "682d1295e0e8b8ba4134dfb1",
-      "name": "HTML",
-      "slug": "html",
-      "icon": "https://imgur.com/b5vSaWO.png",
-      "appIcon": "https://imgur.com/jlT7W6i.png"
-    },
-    {
-      "_id": "682d12a7e0e8b8ba4134dfb7",
-      "name": "Vite",
-      "slug": "vite",
-      "icon": "https://imgur.com/cy5Pbzc.png",
-      "appIcon": "https://imgur.com/Hni4GUX.png"
-    }
-  ]
+  {
+    _id: "682d0b50e0e8b8ba4134df5b",
+    name: "React",
+    slug: "react",
+    icon: reactIcon,
+    appIcon: reactIcon,
+  },
+  {
+    _id: "682d11b0e0e8b8ba4134df99",
+    name: "TS",
+    slug: "ts",
+    icon: typescriptIcon,
+    appIcon: typescriptIcon,
+  },
+  {
+    _id: "682d009c34e57a1daab77345",
+    name: "Redux",
+    slug: "redux",
+    icon: reduxIcon,
+    appIcon: reduxIcon,
+  },
+  {
+    _id: "682cfe1d34e57a1daab7731f",
+    name: "NodeJs",
+    slug: "nodejs",
+    icon: nodeIcon,
+    appIcon: nodeIcon,
+  },
+  {
+    _id: "682d013b34e57a1daab77351",
+    name: "Laravel",
+    slug: "laravel",
+    icon: laravelIcon,
+    appIcon: laravelIcon,
+  },
+  {
+    _id: "682d009c34e57a1daab77346",
+    name: "PHP",
+    slug: "php",
+    icon: phpIcon,
+    appIcon: phpIcon,
+  },
+  {
+    _id: "682d013b34e57a1daab77352",
+    name: "Python",
+    slug: "python",
+    icon: pythonIcon,
+    appIcon: pythonIcon,
+  },
+  {
+    _id: "682d009c34e57a1daab77347",
+    name: "Docker",
+    slug: "docker",
+    icon: dockerIcon,
+    appIcon: dockerIcon,
+  },
+  {
+    _id: "682d009c34e57a1daab77348",
+    name: "Jest",
+    slug: "jest",
+    icon: jestIcon,
+    appIcon: jestIcon,
+  },
+  {
+    _id: "682d009c34e57a1daab77349",
+    name: "JS",
+    slug: "js",
+    icon: javascriptIcon,
+    appIcon: javascriptIcon,
+  },
+  {
+    _id: "682d013b34e57a1daab77353",
+    name: "HTML",
+    slug: "html",
+    icon: htmlIcon,
+    appIcon: htmlIcon,
+  },
+  {
+    _id: "682d0c21e0e8b8ba4134df77",
+    name: "CSS",
+    slug: "css",
+    icon: cssIcon,
+    appIcon: cssIcon,
+  },
+  {
+    _id: "682d0c37e0e8b8ba4134df7d",
+    name: "AWS",
+    slug: "aws",
+    icon: awsIcon,
+    appIcon: awsIcon,
+  },
+  {
+    _id: "682d0d66e0e8b8ba4134df83",
+    name: "PostgreSQL",
+    slug: "postgresql",
+    icon: postgresqlIcon,
+    appIcon: postgresqlIcon,
+  },
+  {
+    _id: "682d0d81e0e8b8ba4134df89",
+    name: "Fastlane",
+    slug: "fastlane",
+    icon: fastlaneIcon,
+    appIcon: fastlaneIcon,
+  },
+  {
+    _id: "682d11dce0e8b8ba4134dfa5",
+    name: "Firebase",
+    slug: "firebase",
+    icon: firebaseIcon,
+    appIcon: firebaseIcon,
+  },
+  {
+    _id: "682d11c5e0e8b8ba4134df9f",
+    name: "Git",
+    slug: "git",
+    icon: gitIcon,
+    appIcon: gitIcon,
+  },
+  {
+    _id: "682d11eae0e8b8ba4134dfab",
+    name: "Figma",
+    slug: "figma",
+    icon: figmaIcon,
+    appIcon: figmaIcon,
+  },
+  {
+    _id: "682d12a7e0e8b8ba4134dfb7",
+    name: "Vite",
+    slug: "vite",
+    icon: viteIcon,
+    appIcon: viteIcon,
+  },
+];
